2334c8f63d73ab6b89d788937a6b5455d475037d
Tymoteusz Gedliczka <tymoteusz.gedliczka@pl.ibm.com>
2021-12-07 07:06:52 +0100

    day 7

---
+++ day7/day7b.py
@@ -0,0 +1,31 @@
+
+def align_cost(dest, positions):
+    moves = list(map(lambda h: sum(range(max(h, dest) - min(h, dest) +1)), positions))
+    total_cost =  sum(moves)
+    # print("cost for " + str(dest) + ": " + str(total_cost))
+    return total_cost
+
+with open('day7/input.txt') as f:
+    line = f.readline().rstrip('\n')
+    crab_positions = list(map(int, line.split(',')))
+    sum_of_crabs = sum(crab_positions)
+    avg = round(sum_of_crabs / len(crab_positions))
+    best_cost = align_cost(avg, crab_positions)
+    step = 0
+    best_pos = avg
+    improved = True
+    while (improved):
+        improved =  False
+        step += 1
+        new_best_cost = align_cost(avg + step, crab_positions)
+        if (new_best_cost < best_cost):
+            improved = True
+            best_cost = new_best_cost
+            best_pos = avg + step
+        new_best_cost = align_cost(avg - step, crab_positions)
+        if (new_best_cost < best_cost):
+            improved = True
+            best_cost = new_best_cost
+            best_pos = avg - step
+    print(best_cost)
+    # print(best_pos)
